8a4cd35e8eee62a94b952a2e55e2c0015f34d6d9
joagranadosme <joagranadosme@unal.edu.co>
2017-11-25 11:29:28 -0500

    save arezzo files

--- Practica3/blureffect.cu
+++ Practica3/blureffect.cu
@@ -1,17 +1,18 @@
 #include <stdio.h>
 #include <iostream>
-#include <cuda_runtime.h>
 #include <opencv2/opencv.hpp>
+#include <cuda.h>
+#include <cuda_runtime.h>
 
 //Namespaces.
 using namespace std;
 using namespace cv;
 
-//Hallar promedio y retornarlo.
-//__global__ int cal_intensity(int x, int y, int k, int c){
-__global__ int cal_intensity(int* image, int x, int y, int width, int height, int k){
 
-	int ic, ir, fc, fr, n, intesity;
+//Hallar promedio y retornarlo.
+__device__ int cal_intensity(const int* image, int x, int y, int width, int height, int k){
+
+	int ic, ir, fc, fr, n;
 	x-(k/2)+1<0 ? ic = 0 : ic = x-(k/2);
 	y-(k/2)+1<0 ? ir = 0 : ir = y-(k/2);
 	x+(k/2)+1>width ? fc = width : fc = x+(k/2)+1;
@@ -20,7 +21,7 @@ __global__ int cal_intensity(int* image, int x, int y, int width, int height, in
 	int red = 0, green = 0, blue = 0;
 	for(int i=ic; i<fc; i++){
     		for(int j=ir; j<fr; j++){
-			n = image[i+j*height];
+			n = image[j+i*height];
 			blue += (n % 1000);
 			green += (n/1000) % 1000;
 			red += (n/1000000) % 1000;
@@ -34,19 +35,25 @@ __global__ int cal_intensity(int* image, int x, int y, int width, int height, in
 }
 
 //Funcion de cada hilo.
-__global__ void blur_thread(const int* d_image, const int width, const int height, const int kernel, const int total_threads, int d_blur){
-
+__global__ void blur_thread(const int* d_image, const int width, const int height, const int kernel, const int total_threads, int* d_blur){
+	
 	int id = blockDim.x * blockIdx.x + threadIdx.x;
-
 	int ir = id * ( height / total_threads );
 	int fr = (id + 1) * ( height / total_threads );
 
-	for(int i=0; i<width; i++){
-		for(int j=ir; j<fr; j++){
-			d_blur[i+j*height] = cal_intesity(d_image, i, j, width, height, kernel);
+	if(id < height){
+		for(int i=0; i<width; i++){
+			for(int j=ir; j<fr; j++){
+				//d_blur[j+i*height] = d_image[j+i*height];;
+				d_blur[j+i*height] = cal_intensity(d_image, i, j, width, height, kernel);
+				//d_blur[j*width+i] = cal_intensity(d_image, i, j, width, height, kernel);
+				//d_blur[j+i*height] = d_image[j+i*height];
+				//d_blur[j+i*height] = d_image[j+i*height];
+			}
 		}
 	}
 }
+
 
 //Main.
 int main(int argc, char** argv){
@@ -54,15 +61,14 @@ int main(int argc, char** argv){
 	//Variables.
 	char* image_name;
 	Mat image, blur_image;
-	int kernel_size, num_threads, num_blocks, width, height;
+	int kernel_size, num_threads, num_blocks;
 		
 	//Recibir argumentos.
 	image_name = argv[1];
 	kernel_size = atoi(argv[2]);
 	num_threads = atoi(argv[3]);
-	num_blocks = atoi(argv[4]);
-
-	if(argc != 5){
+	
+	if(argc != 4){
 		cout<<"Numero incorrecto de argumentos.\n";
 		return -1;
 	}
@@ -75,55 +81,41 @@ int main(int argc, char** argv){
 	}
 
 	//Inicializar variables
-	width = image.cols;
-	height = image.rows;
+	int width = image.cols;
+	int height = image.rows;
 	blur_image = image.clone();
 	cudaError_t err = cudaSuccess;
 
-
 	//Malloc host
-	int numElements = width*height*3;
-	size_t size = numElements*sizeof(int);
+	int numElements = width*height;
+	size_t size = numElements * sizeof(int);
 	int *h_image = (int *)malloc(size);
+	int *h_blur = (int *)malloc(size); 
 
 	//Imagen a un vector 3D
 	int aux = 0;
 	for(int i=0; i<width; i++){
 		for(int j=0; j<height; j++){
-			h_image[aux] = image.at<Vec3b>(i,j)[0];
-			h_image[aux] += image.at<Vec3b>(i,j)[1] * 1000;
-			h_image[aux] += image.at<Vec3b>(i,j)[2] * 1000000;
+			h_image[aux] = image.at<Vec3b>(j,i)[0];
+			h_image[aux] += image.at<Vec3b>(j,i)[1] * 1000;
+			h_image[aux] += image.at<Vec3b>(j,i)[2] * 1000000;
 			aux++;
 		}
 	}
-	
+
 	//Malloc devise
-	//Width
-	int d_width = NULL;
-	err = cudaMalloc((void *)d_width, sizeof(int));
-	if(err != cudaSuccess){
-		cout<<"Error separando espacio en GPU "<<cudaGetErrorString(err))<<endl;
-		return -1;
-	}
-	//Height
-	int d_height = NULL;
-	err = cudaMalloc((void *)d_height, sizeof(int));
-	if(err != cudaSuccess){
-		cout<<"Error separando espacio en GPU "<<cudaGetErrorString(err))<<endl;
-		return -1;
-	}
-	//Kernel
-	int d_kernel = NULL;
-	err = cudaMalloc((void *)d_kernel, sizeof(int));
-	if(err != cudaSuccess){
-		cout<<"Error separando espacio en GPU "<<cudaGetErrorString(err))<<endl;
-		return -1;
-	}
-	//Array
+	//Imagen
 	int *d_image = NULL;
 	err = cudaMalloc((void **)&d_image, size);
 	if(err != cudaSuccess){
-		cout<<"Error separando espacio en GPU "<<cudaGetErrorString(err))<<endl;
+		cout<<"Error separando espacio imagen normal en GPU "<<cudaGetErrorString(err)<<endl;
+		return -1;
+	}
+	//Clon
+	int *d_blur = NULL;
+	err = cudaMalloc((void **)&d_blur, size);
+	if(err != cudaSuccess){
+		cout<<"Error separando espacio imagen difuminada en GPU "<<cudaGetErrorString(err)<<endl;
 		return -1;
 	}
 
@@ -131,40 +123,55 @@ int main(int argc, char** argv){
 	//Imagen
 	err = cudaMemcpy(d_image, h_image, size, cudaMemcpyHostToDevice);
 	if (err != cudaSuccess){
-		cout<<"Error copiando datos a GPU "<<cudaGetErrorString(err))<<endl;
-		return -1;
-	}
-	//Width
-	err = cudaMemcpy(d_width, width, sizeof(int), cudaMemcpyHostToDevice);
-	if (err != cudaSuccess){
-		cout<<"Error copiando datos a GPU "<<cudaGetErrorString(err))<<endl;
-		return -1;
-	}
-	//Kernel
-	err = cudaMemcpy(d_kernel, kernel_size, sizeof(int), cudaMemcpyHostToDevice);
-	if (err != cudaSuccess){
-		cout<<"Error copiando datos a GPU "<<cudaGetErrorString(err))<<endl;
-		return -1;
-	}
-	//Height
-	err = cudaMemcpy(d_height, height, sizeof(int), cudaMemcpyHostToDevice);
-	if (err != cudaSuccess){
-		cout<<"Error copiando datos a GPU "<<cudaGetErrorString(err))<<endl;
+		cout<<"Error copiando datos a GPU "<<cudaGetErrorString(err)<<endl;
 		return -1;
 	}
 
 	//Lanzar GPU
-	printf("CUDA kernel launch with %d blocks of %d threads\n", blocksPerGrid, threadsPerBlock);
-	cout<<"Kernel de CUDA lanzado con "<<num_blocks<<" bloques y "<<num_threads<<" hilos."<<endl;
-	vectorAdd<<<blocksPerGrid, threadsPerBlock>>>(d_A, d_B, d_C, numElements);
+	int blocksPerGrid = (height + num_threads - 1) / num_threads;	
+	blur_thread<<<blocksPerGrid, num_threads>>>(d_image, width, height, kernel_size, height, d_blur);
 	err = cudaGetLastError();
 	if (err != cudaSuccess){
-		cout<<"Fallo al lanzar Kerndel de GPU "<<cudaGetErrorString(err))<<endl;
+		cout<<"Fallo al lanzar Kerndel de GPU "<<cudaGetErrorString(err)<<endl;
+		return -1;
+	}
+	
+	//Copiar de GPU a CPU
+	cout<<"Copiando datos desde la GPU a CPU."<<endl;
+    	err = cudaMemcpy(h_blur, d_blur, size, cudaMemcpyDeviceToHost);
+	if (err != cudaSuccess){
+		cout<<"Error copiando desde GPU a CPU "<<cudaGetErrorString(err)<<endl;
 		return -1;
 	}
 
-
 	//Escribir imagen difuminada.
+	aux = 0;
+	for(int i=0; i<width; i++){
+		for(int j=0; j<height; j++){
+			blur_image.at<Vec3b>(j,i)[0] = (unsigned char)((h_blur[aux]) % 1000);
+			blur_image.at<Vec3b>(j,i)[1] = (unsigned char)((h_blur[aux]/1000) % 1000);
+			blur_image.at<Vec3b>(j,i)[2] = (unsigned char)((h_blur[aux]/1000000) % 1000);
+			aux++;
+		}
+	}
 	imwrite("blur_image.jpg", blur_image);
+	
+
+	//Libear espacio
+	err = cudaFree(d_image);
+	if (err != cudaSuccess){
+	        cout<<"Error liberando memoria de imagen normal "<<cudaGetErrorString(err)<<endl;
+		return -1;
+    	}
+
+	err = cudaFree(d_blur);
+	if (err != cudaSuccess){
+	        cout<<"Error liberando memoria de imagen difuminada "<<cudaGetErrorString(err)<<endl;
+		return -1;
+    	}
+
+	free(h_image);
+	free(h_blur);
+
 	return 0;
 }
--- Practica3/script.sh
+++ Practica3/script.sh
@@ -3,14 +3,14 @@ for k in grumpy720p.jpg grumpy1080p.jpg grumpy4k.jpg
 do
 echo "Imagen" $k
 echo "Imagen" $k >> results.txt
-for i in 1 2 4 8 16
+for i in 80 100 200 400 800 1000
 do
 echo $i hilos
 echo $i hilos >> results.txt
 for j in $(seq 0 15)
 do
 d1=$(date "+%s%N")/1000000
-./blureffect $k 7 $i
+./blureffect $k 21 $i
 d2=$(date "+%s%N")/1000000
 echo $(($d2 - $d1)) >> results.txt
 done
